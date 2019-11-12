200839e5d964c18a4b67bb65df924cf40bc03991
Atharva <atharvamuley1@gmail.com>
2019-11-11 20:05:26 -0500

    vector improvemnets

--- Vector Clocks/node.cpp
+++ Vector Clocks/node.cpp
@@ -4,13 +4,36 @@
 #include <sys/types.h>
 #include <pthread.h>
 #include <fstream>
+#include <algorithm>
 
 #define NO_OF_CLIENTS 3
 
 // Vector clock for all process. Limited to three process
 int vector_clock[NO_OF_CLIENTS];
+int processid = -1;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t lock = PTHREAD_COND_INITIALIZER;
+
+void update_vector_clock(int va[NO_OF_CLIENTS]){
+    pthread_mutex_lock(&mutex);
+    while(!&lock){
+        pthread_cond_wait(&lock,&mutex);
+    }
+
+    for(int i=0; i< NO_OF_CLIENTS;i++){
+        if (processid == i){
+            vector_clock[i] == std::max(vector_clock[i], va[i]) + 1;
+        }
+        else{
+            vector_clock[i] == std::max(vector_clock[i], va[i]);
+        }
+    }
+    pthread_cond_signal(&lock);
+    pthread_mutex_unlock(&mutex);
+}
 
 struct thread_data{
+    // int processid;
     int sockfd;
     std::string filename;
 };
@@ -50,6 +73,7 @@ int SocketInit(int port){
 /* this thread is used for sending data to other processes */
 void* send_thread(void * args){
     struct thread_data data = *((struct thread_data*)args);
+
     std::ifstream readfile;
     readfile.open(data.filename);
     std::string output;
@@ -71,15 +95,14 @@ int main(int argc, char *argv[]){
         std::cout<< "Please enter process number" << std::endl;
         return -1;
     }
-    int process_no = std::stoi(argv[1]);
-
     struct thread_data td;
+    processid = std::stoi(argv[1]);
     td.sockfd = SocketInit(8824);
-    td.filename = std::to_string(process_no) + ".txt";
+    td.filename = std::to_string(processid) + ".txt";
     pthread_t send_t, recv_t;
 
     pthread_create(&send_t,NULL, send_thread, (void *)&td);
-    pthread_create(&recv_t,NULL, recv_thread, (void *)NULL);
+    pthread_create(&recv_t,NULL, recv_thread, (void *)td);
 
 
     pthread_join(send_t, NULL);
