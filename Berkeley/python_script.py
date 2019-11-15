e8bba17d18522d59071f7c118cb1f4f534ee00aa
Atharva <atharvamuley1@gmail.com>
2019-11-15 11:57:15 -0500

    script

--- Berkeley/python_script.py
+++ Berkeley/python_script.py
@@ -1,3 +1,5 @@
 import os 
 
-os.system("gnome-terminal")
+os.system(" exec gnome-terminal --command=./berkeley_time_demon")
+os.system(" exec gnome-terminal --command=./node")
+os.system(" exec gnome-terminal --command=./node")
